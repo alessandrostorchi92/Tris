2b8622f7e8e7027ffb4bfc698cff16a02faa8de1
alessandrostorchi92 <storchi.alle@gmail.com>
2024-01-02 22:02:35 +0100

    checking of players's moves

--- js/main.js
+++ js/main.js
@@ -20,6 +20,23 @@ const playingUser = document.querySelector(".playing-user");
 
 let userPlaying = 1;
 
+let user1Moves = [];
+let user2Moves = [];
+const possibleSolutions = [
+  // Orizzontale
+  "0,1,2",
+  "3,4,5",
+  "6,7,8",
+  // Verticale
+  "0,3,6",
+  "1,4,7",
+  "2,5,8",
+  // Diagonale
+  "0,4,8",
+  "6,4,2",
+];
+
+
 
 /* ----------
 
@@ -40,7 +57,7 @@ createGrid(trisGridContainer);
  */
 function createGrid(container) {
 
-    for(let i = 1; i < 10; i++) {
+    for(let i = 0; i < 9; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.dataset.numberSquare = i.toString();
@@ -54,7 +71,6 @@ function createGrid(container) {
 
 }
 
-
 // TODO: Setting up of the features of the addEventListener
 /**
  * This function activates features once you click on the square
@@ -67,16 +83,34 @@ function onSquareClick() {
         return;
     }
 
-    console.log(this.dataset.numberSquare);
-    console.log(this.dataset.user);
+    //? console.log(this.dataset.numberSquare); OK!
+    //? console.log(this.dataset.user);OK!
+
+    // Initialisation of the variable without assigning it any initial value.
+    let result;
     
     if(userPlaying === 1){
         this.classList.add("clicked-square-player1");
         this.innerHTML = "<i class='fas fa-xmark'>";
+
+        user1Moves.push(this.dataset.numberSquare);
+        console.log(user1Moves);
+        result = checkResult(user1Moves);
+
     }else{
         this.classList.add("clicked-square-player2");
         this.innerHTML = "<i class='fa-solid fa-o'></i>";
+
+        user2Moves.push(this.dataset.numberSquare);
+        console.log(user2Moves);
+        result = checkResult(user2Moves);
     }
+
+    if (result) {
+        console.log(`L'utente ${userPlaying} ha vinto!`);
+        return;
+    }
+    console.log("Result:", result);
 
     this.dataset.user = userPlaying.toString();
     updatePlayingUser(true);
@@ -98,4 +132,27 @@ function updatePlayingUser(changePlayer) {
         }
     }
     playingUser.textContent = "Turn of the player" + " " + userPlaying;
+}
+
+// TODO: checking of players's moves
+
+/**
+ * This function checks if a player won
+ * @param {string[]} userMoves These represent the moves made by each player
+ * @returns {boolean}
+ */
+
+function checkResult(userMoves) {
+    const moves = userMoves.sort().join();
+    let hasWon = false;
+
+    for(let i = 0; i < possibleSolutions.length; i++ ) {
+        const solutions = possibleSolutions[i];
+
+        if(moves.includes(solutions)) {
+            hasWon = true;
+        }
+    }
+
+    return hasWon
 }
